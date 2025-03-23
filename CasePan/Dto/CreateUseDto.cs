e20dbcb2962ba8a0cedbd3d18b4b54e1182cc29c
YuriLopess <costalopesyuri@gmail.com>
2025-03-23 13:38:33 -0300

    feat(dto): add CreateUserDTO for user registration

---
+++ CasePan/Dto/CreateUseDto.cs
@@ -0,0 +1,9 @@
+namespace CasePan.Data
+{
+    public class CreateUserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
