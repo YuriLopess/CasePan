f06cf88db6e59cd480461bf97552f153063c4594
YuriLopess <costalopesyuri@gmail.com>
2025-03-23 09:13:31 -0300

    feat(models): add Response entity and refine User entity methodology

---
+++ CasePan/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace CasePan.Models
+{
+    public class Response
+    {
+        public string Message { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+    }
+}
