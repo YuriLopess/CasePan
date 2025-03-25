86986973b4a0550ff08f993d9cf24e51fb4bd6a1
YuriLopess <costalopesyuri@gmail.com>
2025-03-25 05:15:25 -0300

    refactor: change the set field to init in non-mutable values after creation

--- CasePan/Models/UserModel.cs
+++ CasePan/Models/UserModel.cs
@@ -2,11 +2,11 @@
 {
     public class UserModel
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; init; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
         public DateTime LastModifiedAt { get; set; } = DateTime.UtcNow;
         public bool IsActive { get; set; } = true;
     }
